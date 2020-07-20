8242bc32aff322465380102f6524fbe8b2414098
wuhao <18365298803@163.com>
2020-07-20 10:38:08 +0800

    使用Turbine实现集群监控

--- hystrix-feign-demo/src/main/java/com/example/hystrixfeigndemo/HystrixFeignDemoApplication.java
+++ hystrix-feign-demo/src/main/java/com/example/hystrixfeigndemo/HystrixFeignDemoApplication.java
@@ -2,16 +2,17 @@ package com.example.hystrixfeigndemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 // 在启动类上添加 @EnableHystrix 或者 @EnableCircuitBreaker。注意，@EnableHystrix 中包含了 @EnableCircuitBreaker。
 @EnableHystrix
 @EnableDiscoveryClient
-@EnableFeignClients
+@EnableHystrixDashboard
+@EnableFeignClients//(basePackages="com.example.hystrixfeigndemo.interfaces")
 public class HystrixFeignDemoApplication {
 
     public static void main(String[] args) {
--- ribbon-native-demo/src/main/java/com/example/ribbonnativedemo/RibbonNativeDemoApplication.java
+++ ribbon-native-demo/src/main/java/com/example/ribbonnativedemo/RibbonNativeDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 //在启动类上加 @EnableFeignClients 注解，如果你的 Feign 接口定义跟你的启动类不在同一个包名下，
 // 还需要制定扫描的包名 @EnableFeignClients（basePackages=“com.fangjia.api.client”），
-@EnableFeignClients
+@EnableFeignClients//(basePackages="com.example.ribbonnativedemo.interfaces")
 public class RibbonNativeDemoApplication {
 
     public static void main(String[] args) {
